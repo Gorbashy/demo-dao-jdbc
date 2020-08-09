eeeea040a9318484931137d4f86bbaa0e0e70e91
gorgashy <james.freire@wapnet.com.br>
2020-08-09 16:36:00 -0300

    Reusing instantiation

--- src/model/dao/SellerDao.java
+++ src/model/dao/SellerDao.java
@@ -5,9 +5,10 @@ import java.util.List;
 import model.entities.Seller;
 
 public interface SellerDao {
-	void update (Seller obj);
-	void deletById (Integer id);
-	Seller findById (Integer id);
+
+	void insert(Seller obj);
+	void update(Seller obj);
+	void deleteById(Integer id);
+	Seller findById(Integer id);
 	List<Seller> findAll();
 }
-
